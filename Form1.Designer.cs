a3ac50f9a7766383cb70611e62b82e953005fa4a
EmperoRRx <85365315+EmperoRRx@users.noreply.github.com>
2021-06-04 20:01:43 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Zadanie_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Stop = new System.Windows.Forms.Button();
+            this.ScoreBox = new System.Windows.Forms.TextBox();
+            this.LevelOfDifficulty = new System.Windows.Forms.TrackBar();
+            this.Start = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.PlayerNicknameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.GameName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LevelOfDifficulty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Stop.Font = new System.Drawing.Font("Agency FB", 22.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(558, 509);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(122, 80);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Visible = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // ScoreBox
+            // 
+            this.ScoreBox.Location = new System.Drawing.Point(724, 557);
+            this.ScoreBox.Name = "ScoreBox";
+            this.ScoreBox.ReadOnly = true;
+            this.ScoreBox.Size = new System.Drawing.Size(68, 22);
+            this.ScoreBox.TabIndex = 2;
+            this.ScoreBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ScoreBox.Visible = false;
+            this.ScoreBox.TextChanged += new System.EventHandler(this.ScoreBox_TextChanged);
+            // 
+            // LevelOfDifficulty
+            // 
+            this.LevelOfDifficulty.BackColor = System.Drawing.SystemColors.Desktop;
+            this.LevelOfDifficulty.Location = new System.Drawing.Point(31, 533);
+            this.LevelOfDifficulty.Maximum = 3;
+            this.LevelOfDifficulty.Name = "LevelOfDifficulty";
+            this.LevelOfDifficulty.Size = new System.Drawing.Size(222, 56);
+            this.LevelOfDifficulty.TabIndex = 4;
+            this.LevelOfDifficulty.Visible = false;
+            this.LevelOfDifficulty.Scroll += new System.EventHandler(this.LevelOfDifficulty_Scroll);
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Start.Font = new System.Drawing.Font("Agency FB", 22.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(314, 194);
+            this.Start.Name = "Start";
+            this.Start.Padding = new System.Windows.Forms.Padding(1);
+            this.Start.Size = new System.Drawing.Size(166, 80);
+            this.Start.TabIndex = 5;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 3000;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // PlayerNicknameBox
+            // 
+            this.PlayerNicknameBox.Location = new System.Drawing.Point(314, 347);
+            this.PlayerNicknameBox.Name = "PlayerNicknameBox";
+            this.PlayerNicknameBox.Size = new System.Drawing.Size(166, 22);
+            this.PlayerNicknameBox.TabIndex = 6;
+            this.PlayerNicknameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 513);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Easy";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 513);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Normal";
+            this.label2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(202, 513);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Very Hard";
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(152, 513);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Hard";
+            this.label3.Visible = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ScoreLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(731, 533);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(52, 17);
+            this.ScoreLabel.TabIndex = 13;
+            this.ScoreLabel.Text = "Score";
+            this.ScoreLabel.Visible = false;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.NameLabel.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(294, 293);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(205, 36);
+            this.NameLabel.TabIndex = 14;
+            this.NameLabel.Text = "Enter your nickname:";
+            // 
+            // GameName
+            // 
+            this.GameName.AutoSize = true;
+            this.GameName.BackColor = System.Drawing.SystemColors.Desktop;
+            this.GameName.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameName.Location = new System.Drawing.Point(212, 9);
+            this.GameName.Name = "GameName";
+            this.GameName.Size = new System.Drawing.Size(366, 73);
+            this.GameName.TabIndex = 15;
+            this.GameName.Text = "DuckShooter2021";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = global::Zadanie_2.Properties.Resources.Webp_net_compress_image__6_;
+            this.ClientSize = new System.Drawing.Size(836, 601);
+            this.Controls.Add(this.GameName);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PlayerNicknameBox);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.LevelOfDifficulty);
+            this.Controls.Add(this.ScoreBox);
+            this.Controls.Add(this.Stop);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "DuckShooter2021";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LevelOfDifficulty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.TextBox ScoreBox;
+        private System.Windows.Forms.TrackBar LevelOfDifficulty;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.TextBox PlayerNicknameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label GameName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_2
+{    
+    public partial class Form1 : Form
+    {
+        class Duck : Button//Klasa kaczki dziedzicząca z buttona.
+        {
+            private bool is_shooted;
+
+            public Duck()//Konstruktor.
+            {
+                is_shooted = false;
+                //Text = "DUCK"; Jak jest załadowany obrazek to niepotrzebnie.
+            }
+
+            public bool IsShooted//Getter is_shooted.
+            {
+                get 
+                {
+                    return is_shooted; 
+                }
+            }
+
+            public void Shoot()//Zestrzelenie kaczki.
+            {
+                is_shooted = true;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Zmienne globalne gry.
+        public int score = 0;
+        public int total_score = 0;
+        public string nickname = "";
+        public int duck_number = 1;       
+       
+        readonly Random generator = new Random();
+        readonly Random time = new Random();
+        readonly List<Duck> ducks = new List<Duck>();
+
+
+     
+
+
+
+
+        private void RemoveDucks()//Metoda usuwająca kaczki z planszy.
+        {
+            if (ducks.Count != 0)
+            {
+                foreach (Duck dead_duck in ducks)
+                {
+                    Controls.Remove(dead_duck);
+                    dead_duck.Dispose();
+                }
+            }
+        }
+
+        private void CreateDuck()//Tworzenie jednej kaczki , ustawianie jej parametrów.
+        {
+            Duck duck = new Duck();
+
+            duck.Click += (sender, e) => AddPoint(sender, e, duck);//Inny event_holder pozwalający przekazać parametry.
+
+
+            Point location = new Point(generator.Next(10, 500), generator.Next(1, 340));
+            duck.Location = location;
+            duck.Size = new Size(77, 60);
+
+            
+            duck.Image = Image.FromFile("Duck.jpg");
+
+            Controls.Add(duck);
+            ducks.Add(duck);                       
+        }
+      
+        private void SpawnDucks()//Metoda tworząca i zliczająca kaczki.
+        {
+            for (int i = 0; i < duck_number; i++)
+            {
+                CreateDuck();
+                total_score++;
+            }
+        }
+        
+        private void AddPoint(object sender, EventArgs e, Duck duck)//Metoda dodająca punkt za zestrzeloną kaczkę.
+        {
+            if (!duck.IsShooted)
+            {
+                score++;
+                SetScore();
+                duck.Shoot();
+                duck.Visible = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void SetScore()//Metoda ustawiająca wynik.
+        {
+            ScoreBox.Text = "";
+            ScoreBox.Text =   score.ToString() + "/" + total_score.ToString();
+        }
+
+        private void LevelControl()//Metoda sprawdzająca poziom gry.
+        {           
+            if (LevelOfDifficulty.Value == 0)
+            {
+                GameTimer.Interval = time.Next(3000, 3601);
+                duck_number = 1;
+            }
+            else if (LevelOfDifficulty.Value == 1)
+            {
+                GameTimer.Interval = time.Next(1500, 1800);
+                duck_number = generator.Next(1, 3);
+            }
+            else if (LevelOfDifficulty.Value == 2)
+            {
+                GameTimer.Interval = time.Next(1000, 1201);
+                duck_number = generator.Next(2, 4);
+            }
+            else if (LevelOfDifficulty.Value == 3)
+            {
+                GameTimer.Interval = time.Next(1000, 1101);
+                duck_number = generator.Next(2, 5);
+            }
+        }
+        
+        private void SaveToFile()//Metoda zapisująca wynik,date oraz nazwę użytnownika do pliku txt.
+        {
+            using (StreamWriter score_after_game = new StreamWriter("Wyniki.txt", true))
+            {
+                string final_score_form = "";
+                foreach (char letter in ScoreBox.Text)
+                {
+                    if (letter != ' ')
+                    {
+                        final_score_form += letter;
+                    }
+                }
+                string date = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss tt");
+                score_after_game.WriteLine(date.ToString() + " |" + nickname + "| " + final_score_form);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void Start_Click(object sender, EventArgs e)//Metoda wywołana po kliknęciu w Start.
+        {
+            nickname = PlayerNicknameBox.Text;
+
+            if (String.IsNullOrEmpty(PlayerNicknameBox.Text))//Sprawdzamy czy użytkownik podał nazwę.
+            {
+                MessageBox.Show("Podaj nazwę gracza.", "Error");
+            }
+            else
+            {
+                PlayerNicknameBox.Text = "";//Zmieniamy "okienko" na okno gry.
+                Start.Visible = false;
+                PlayerNicknameBox.Visible = false;
+                NameLabel.Visible = false;
+                GameName.Visible = false;
+
+                LevelOfDifficulty.Visible = true;
+                Stop.Visible = true;
+                ScoreBox.Visible = true;
+                GameTimer.Enabled = true;
+                 
+                ScoreLabel.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)//Metoda wywołana po kliknięciu w STOP.
+        {
+            SaveToFile();
+            RemoveDucks();
+            
+            score = 0;//Zmieniamy "okienko" na okno startu oraz czyścimy wszystkie zmienne.
+            total_score = 0;
+            LevelOfDifficulty.Value = 0;
+            duck_number = 1;
+            GameTimer.Interval = 3000;
+
+            nickname = "";
+            ScoreBox.Text = "";
+                                
+            
+            Start.Visible = true;
+            PlayerNicknameBox.Visible = true;
+            NameLabel.Visible = true;
+            GameName.Visible = true;
+
+
+
+            LevelOfDifficulty.Visible = false;
+            Stop.Visible = false;
+            ScoreBox.Visible = false;
+            GameTimer.Enabled = false;
+
+            ScoreLabel.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)//Metoda wywołana po ticku GameTimera(3000ms startowo).
+        {            
+            RemoveDucks();
+            ducks.Clear();
+            SpawnDucks();
+            SetScore();
+            LevelControl();
+        }
+
+        private void LevelOfDifficulty_Scroll(object sender, EventArgs e)//Metoda wywołana po zmianie poziomu.
+        {
+            LevelControl();
+        }
+
+
+
+
+
+
+
+
+        private void ScoreBox_TextChanged(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }             
+    }
+}
